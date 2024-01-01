ba443869126315c8c6056adfe352b0782c7bf12d
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2024-01-02 01:56:09 +0800

    戳一戳语音终于可以用了

--- apps/派蒙戳一戳.js
+++ apps/派蒙戳一戳.js
@@ -39,9 +39,9 @@ let errors = {}
 
 
 
-let reply_text = 0.0 //文字触发概率,精确到小数点后15位
+let reply_text = 0.6 //文字触发概率,精确到小数点后15位
 let reply_img = 0.15 //随机图片触发概率
-let reply_voice = 0.7 //语音触发概率
+let reply_voice = 0.1 //语音触发概率
 let mutepick = 0.05 //禁言触发概率
 // 剩下的0.1概率是机器人戳回去
 
