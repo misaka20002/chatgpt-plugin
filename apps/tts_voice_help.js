cd542803cef2a1eefbf0cd4a1837c9ff4cd53d58
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2024-05-16 20:40:01 +0800

    Fish-Vits新增BA角色语音

--- apps/tts_voice_help.js
+++ apps/tts_voice_help.js
@@ -8,6 +8,7 @@ import {
     speakers_ZH,
     speakers_JP,
     speakers_EN,
+    speakers_BA,
     vits_emotion_map
 } from '../utils/tts.js'
 import crypto from 'crypto'
@@ -192,7 +193,7 @@ ${userSetting.useTTS === true ? '当前语音模式为' + Config.ttsMode : ''}`
     async tts_show_speakers(e) {
         let strs = [`tts可选人物列表：\n#chatgpt设置全局vits语音角色派蒙_ZH\n#chatgpt设置语音角色可莉_ZH\n  （↑每名用户都可以单独设置）`]
         strs.push(`` +
-            `  支持日语语言，若#tts语音转日语关闭 则自动使用网址api的转日语功能（推荐关闭，除非网址api翻译出错），若#tts语音转日语开启 则使用本插件内置的#gpt翻日 功能（需要设置翻译源）。`)
+            `支持日语语言，若#tts语音转日语关闭 则自动使用网址api的转日语功能（推荐关闭，除非网址api翻译出错），若#tts语音转日语开启 则使用本插件内置的#gpt翻日 功能（需要设置翻译源）。`)
         let batchSpeakersNum = speakers_ZH.length / 50
         let speakersSliced
         let strsLenght = strs.length
@@ -229,6 +230,21 @@ ${userSetting.useTTS === true ? '当前语音模式为' + Config.ttsMode : ''}`
         for (let i = 0; i < batchSpeakersNum; i++) {
             speakersSliced = speakers_EN.slice(i * 50, (i + 1) * 50)
             strs[i + strsLenght] = '英文EN角色：\n'
+            for (let j = 0; j < speakersSliced.length; j++) {
+                if (j % 2 == 0) {
+                    strs[i + strsLenght] += speakersSliced[j] + "　　"
+                }
+                else {
+                    strs[i + strsLenght] += speakersSliced[j] + "\n"
+                }
+            }
+        }
+
+        batchSpeakersNum = speakers_BA.length / 50
+        strsLenght = strs.length
+        for (let i = 0; i < batchSpeakersNum; i++) {
+            speakersSliced = speakers_BA.slice(i * 50, (i + 1) * 50)
+            strs[i + strsLenght] = 'BA角色：\n'
             for (let j = 0; j < speakersSliced.length; j++) {
                 if (j % 2 == 0) {
                     strs[i + strsLenght] += speakersSliced[j] + "　　"
