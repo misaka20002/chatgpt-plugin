c647ee99e9b0432dd670aeb74fbc44ed9431dff3
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2025-02-03 19:36:13 +0800

    add more guoba help

--- guoba.support.js
+++ guoba.support.js
@@ -616,7 +616,7 @@ export function supportGuoba() {
         {
           field: 'geminiModel',
           label: '模型',
-          bottomHelpMessage: '默认值：gemini-pro；可选（注意配额）：gemini-1.5-flash；gemini-1.5-pro；gemini-1.5-flash-8b；learnlm-1.5-pro-experimental；gemini-exp-1121；gemini-exp-1206；gemma-2-2b-it；gemma-2-9b-it；gemma-2-27b-it',
+          bottomHelpMessage: '默认值：gemini-pro；可选（注意配额）：gemini-1.5-flash；gemini-1.5-pro；gemini-1.5-flash-8b；learnlm-1.5-pro-experimental；gemini-2.0-flash-exp；gemini-exp-1206；gemini-2.0-flash-thinking-exp-01-21；gemma-2-2b-it；gemma-2-9b-it；gemma-2-27b-it',
           component: 'Input'
         },
         {
