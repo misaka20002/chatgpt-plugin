7b2262acc6ce3416e6de8067c54bec112a0dd92e
ikechan8370 <geyinchibuaa@gmail.com>
2025-04-23 11:26:43 +0800

    fix: temperature

--- utils/openai/chatgpt-api.js
+++ utils/openai/chatgpt-api.js
@@ -88,7 +88,7 @@ var ChatGPTAPI = /** @class */ (function () {
         this._apiBaseUrl = apiBaseUrl;
         this._debug = !!debug;
         this._fetch = fetch;
-        this._completionParams = __assign({ model: CHATGPT_MODEL, temperature: 0.8, top_p: 1.0, presence_penalty: 1.0 }, completionParams);
+        this._completionParams = __assign({ model: CHATGPT_MODEL, temperature: 1, top_p: 1.0, presence_penalty: 1.0 }, completionParams);
         this._systemMessage = systemMessage;
         if (this._systemMessage === undefined) {
             var currentDate = new Date().toISOString().split('T')[0];
