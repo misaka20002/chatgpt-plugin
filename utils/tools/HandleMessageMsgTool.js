9f80f142f088dc5293cd81803b0cf4716b73ed13
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2025-06-09 10:35:12 +0800

    add dev help

--- utils/tools/HandleMessageMsgTool.js
+++ utils/tools/HandleMessageMsgTool.js
@@ -20,7 +20,13 @@ export class HandleMessageMsgTool extends AbstractTool {
 
   func = async function (opts, e) {
     let { type = 'recall', messageId = e.message_id } = opts
-    messageId = e.source_message_id || e.message_id // 因为 gemini 太蠢了所以手动指定使用 source_message_id
+
+    // 因为 gemini 太蠢了所以手动指定使用 source_message_id
+    if (e.source_message_id && messageId == e.source_message_id)
+      logger.mark("[ChatGPT][handleMsg] ai 已正确选择引用消息 source_message_id")
+    else
+      messageId = e.source_message_id || e.message_id
+
     try {
       switch (type) {
         case 'recall': {
