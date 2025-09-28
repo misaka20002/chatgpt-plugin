97c337e5be29ccd6ba4ecfa81324b245d6b040ec
127W <1274925976@qq.com>
2025-09-28 17:11:51 +0800

    fix priority

--- apps/派蒙meme.js
+++ apps/派蒙meme.js
@@ -95,7 +95,7 @@ export class memes extends plugin {
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
-      priority: 5000,
+      priority: 50,
       rule: [
         {
           /** 命令正则匹配 */
